828195056b2c35498e4f62a6250bff03fdd156b8
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2023-02-18 09:57:14 +0900

    Update defaut config key

--- tests/vlogging/test___init__.py
+++ tests/vlogging/test___init__.py
@@ -6,6 +6,12 @@
 
 import pytest
 import vlogging
+from vlogging import (
+    DEFAUT_FORMAT,
+    DEFAUT_FILTER,
+    DEFAUT_HANDLER,
+    DEFAUT_LOGGER
+)
 
 def test_vlogging():
     print(type(vlogging))
@@ -23,7 +29,7 @@ def test_vlogging():
 
 
 @pytest.mark.parametrize("name, expected", [
-    ("vlogging", "vlogging"),
+    (DEFAUT_LOGGER, DEFAUT_LOGGER),
     (__name__, __name__),
     (None, "root")
 ])
@@ -59,46 +65,46 @@ def test_config_configure(vconfig, config):
 @pytest.mark.parametrize("config", [
     (None),
     ({}),
-    ({"formatters": {"defaut_format": None}})
+    ({"formatters": {DEFAUT_FORMAT: None}})
 ])
 def test_config_configureFormatters(vconfig, config):
     cfg = vconfig.configureFormatters(config)
-    assert cfg.get("defaut_format") is not None
+    assert cfg.get(DEFAUT_FORMAT) is not None
 
 
 @pytest.mark.parametrize("config", [
     (None),
     ({}),
-    ({"filters": {"defaut_filter": None}})
+    ({"filters": {DEFAUT_FILTER: None}})
 ])
 def test_config_configureFilters(vconfig, config):
     cfg = vconfig.configureFilters(config)
-    assert cfg.get("defaut_filter", None) is None
+    assert cfg.get(DEFAUT_FILTER, None) is None
 
 
 @pytest.mark.parametrize("config", [
     (None),
     ({}),
-    ({"handlers": {"defaut_handler": None}})
+    ({"handlers": {DEFAUT_HANDLER: None}})
 ])
 def test_config_configureHandlers(vconfig, config):
     cfg = vconfig.configureHandlers(config)
-    assert cfg.get("defaut_handler") is not None
+    assert cfg.get(DEFAUT_HANDLER) is not None
 
 
 @pytest.mark.parametrize("config", [
     (None),
     ({}),
-    ({"loggers": {"vlogging": None}})
+    ({"loggers": {DEFAUT_LOGGER: None}})
 ])
 def test_config_configureLoggers(vconfig, config):
     cfg = vconfig.configureLoggers(config)
-    assert cfg.get("vlogging") is not None
+    assert cfg.get(DEFAUT_LOGGER) is not None
 
 
 @pytest.mark.parametrize("config, key, defaut", [
-    ({"handlers": {"defaut_handler": None}}, "handlers", "defaut_handler"),
-    ({"loggers": {"vlogging": None}},        "loggers",  "vlogging"),
+    ({"handlers": {DEFAUT_HANDLER: None}}, "handlers", DEFAUT_HANDLER),
+    ({"loggers": {DEFAUT_LOGGER: None}},   "loggers",  DEFAUT_LOGGER),
 ])
 def test_config_configureItems(vconfig, config, key, defaut):
     cfg = vconfig.configureItems(config, key, defaut)
@@ -106,10 +112,10 @@ def test_config_configureItems(vconfig, config, key, defaut):
 
 
 @pytest.mark.parametrize("key, defaut", [
-    ("formatters", "defaut_format"),
-    ("filters",     None),
-    ("handlers",   "defaut_handler"),
-    ("loggers",    "vlogging"),
+    ("formatters", DEFAUT_FORMAT),
+    ("filters",    None),
+    ("handlers",   DEFAUT_HANDLER),
+    ("loggers",    DEFAUT_LOGGER),
 ])
 def test_config_configureDefautConfig(vconfig, key, defaut):
     cfg = vconfig.configureDefautConfig(key, defaut)
